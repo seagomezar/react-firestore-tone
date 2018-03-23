e82cdc1a7268d89023ab68910bea8829f684506b
Sebastian Gomez <seagomezar@gmaill.com>
2018-03-23 13:02:57 -0500

    Implementing paintPiano with same code and implementing and using firestore

--- src/App.js
+++ src/App.js
@@ -1,51 +1,68 @@
 import React, { Component } from 'react';
 import './App.css';
 import Tone from 'tone';
+import database from './firebase';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      allNotes: null
+    };
+
+    this.databaseRef = database.collection("notes").doc("Re0fdrkQs2J1VdRmesgV");
+
+  }
   componentDidMount() {
 
-    const allNotes = ["C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1", 
-    "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", 
-    "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", 
-    "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", 
-    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", 
-    "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6", 
-    "C7" ];
+    this.databaseRef.get().then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data().notes);
+      } else {
+        console.log("No such document!");
+      }
+    }).catch((error) => {
+      console.log("Error getting document:", error);
+    });
 
+  }
+
+  paintPiano() {
+    
     const piano = document.getElementById("piano");
 
-    const widthPerNote = window.innerWidth / (allNotes.length - 25);
+    const widthPerNote = window.innerWidth / (this.state.allNotes.length - 25);
     const heightPerNote = 120;
 
     const blackNotesWidthPortion = 2;
     const blackNotesHeightPortion = 1.6;
 
-    allNotes.forEach((note) => {
+    this.state.allNotes.forEach((note) => {
 
-        const node = document.createElement("li");
-        node.setAttribute("id", note);
+      const node = document.createElement("li");
+      node.setAttribute("id", note);
 
-        if (~note.indexOf("#")) {
-            node.className = "black";
-            node.style.width = (widthPerNote / blackNotesWidthPortion) + "px";
-            node.style.height = (heightPerNote / blackNotesHeightPortion) + "px";
-        } else {
-            node.className = "white";
-            node.style.width = widthPerNote + "px";
-            node.style.height = heightPerNote + "px";
-        }
+      if (~note.indexOf("#")) {
+        node.className = "black";
+        node.style.width = (widthPerNote / blackNotesWidthPortion) + "px";
+        node.style.height = (heightPerNote / blackNotesHeightPortion) + "px";
+      } else {
+        node.className = "white";
+        node.style.width = widthPerNote + "px";
+        node.style.height = heightPerNote + "px";
+      }
 
-        piano.appendChild(node);
+      piano.appendChild(node);
     });
+
   }
 
   render() {
     return (
-        <div>
-            <ul id="piano"></ul>
-        </div>
+      <div>
+        <ul id="piano"></ul>
+      </div>
     );
   }
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,17 @@
 import firebase from 'firebase';
+import 'firebase/firestore';
 
 // Initialize Firebase
 const config = {
+    apiKey: "AIzaSyBOZKJ3I0CD34rr-e0z8BByCe_-be4Avyw",
+    authDomain: "music-generator-b6934.firebaseapp.com",
+    databaseURL: "https://music-generator-b6934.firebaseio.com",
+    projectId: "music-generator-b6934",
+    storageBucket: "music-generator-b6934.appspot.com",
+    messagingSenderId: "404958676896"
 };
 firebase.initializeApp(config);
 
-let database = firebase.database();
+let database = firebase.firestore();
 
 export default database;
